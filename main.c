9a2516b3952dc3f98186ed520f738bd889fdc9bd
jhandryyyy <jhandry.orozco@udla.edu.ec>
2025-11-29 21:39:06 -0500

    Update main.c

--- main.c
+++ main.c
@@ -12,53 +12,60 @@ int main() {
     int opcion;
 
     do {
-        printf("\n===== MENU =====\n");
-        printf("1. Ingresar producto\n");
-        printf("2. Editar producto\n");
-        printf("3. Eliminar producto\n");
-        printf("4. Mostrar productos\n");
-        printf("5. Calcular tiempo total\n");
-        printf("6. Calcular recursos totales\n");
-        printf("0. Salir\n");
-        opcion = leerEnteroPositivo("Seleccione opcion: ");
+    printf("\n===== MENU =====\n");
+    printf("1. Ingresar producto\n");
+    printf("2. Editar producto\n");
+    printf("3. Eliminar producto\n");
+    printf("4. Mostrar productos\n");
+    printf("5. Calcular tiempo total\n");
+    printf("6. Calcular recursos totales\n");
+    printf("7. Vender producto\n");
+    printf("0. Salir\n");
 
-        switch (opcion) {
+    opcion = leerEnteroPositivo("Seleccione opcion: ");
 
-            case 1:
-                ingresarProducto(nombres, tiempo_unitario, recursos_unitarios, demanda, &numProductos);
-                break;
+    switch (opcion) {
 
-            case 2:
-                editarProducto(nombres, tiempo_unitario, recursos_unitarios, demanda, numProductos);
-                break;
+        case 1:
+            ingresarProducto(nombres, tiempo_unitario, recursos_unitarios, demanda, &numProductos);
+            break;
 
-            case 3:
-                eliminarProducto(nombres, tiempo_unitario, recursos_unitarios, demanda, &numProductos);
-                break;
+        case 2:
+            editarProducto(nombres, tiempo_unitario, recursos_unitarios, demanda, numProductos);
+            break;
 
-            case 4:
-                mostrarProductos(nombres, tiempo_unitario, recursos_unitarios, demanda, numProductos);
-                break;
+        case 3:
+            eliminarProducto(nombres, tiempo_unitario, recursos_unitarios, demanda, &numProductos);
+            break;
 
-            case 5:
-                printf("Tiempo total: %d\n", 
-                       calcularTiempo(tiempo_unitario, demanda, numProductos));
-                break;
+        case 4:
+            mostrarProductos(nombres, tiempo_unitario, recursos_unitarios, demanda, numProductos);
+            break;
 
-            case 6:
-                printf("Recursos totales: %d\n", 
-                       calcularRecursos(recursos_unitarios, demanda, numProductos));
-                break;
+        case 5:
+            printf("Tiempo total: %d\n", 
+                   calcularTiempo(tiempo_unitario, demanda, numProductos));
+            break;
 
-            case 0:
-                printf("Saliendo...\n");
-                break;
+        case 6:
+            printf("Recursos totales: %d\n", 
+                   calcularRecursos(recursos_unitarios, demanda, numProductos));
+            break;
 
-            default:
-                printf("Opcion invalida.\n");
-        }
+        case 7:
+            venderProducto(nombres, tiempo_unitario, recursos_unitarios, demanda, numProductos);
+            break;
 
-    } while (opcion != 0);
+        case 0:
+            printf("Saliendo...\n");
+            break;
+
+        default:
+            printf("Opcion invalida.\n");
+    }
+
+} while (opcion != 0);
 
     return 0;
+}
 }
